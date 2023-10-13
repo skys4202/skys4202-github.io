5b2ad4ea313b5fad556ccddd10b37c90b0e2a748
skys4202 <skys4202@naver.com>
2023-10-13 21:36:06 +0900

    JS - HOME

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener('scroll', () =>{
-    console.log(window.scrollY);
     if(window.scrollY > headerHeight){
         header.classList.add('header--dark');
     } else{
         header.classList.remove('header--dark');
     }
+});
+
+
+//HOME 섹션을 아래로 스크롤시 투명하게 처리
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', ()=> {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
